9b2246719628887eb54c6bd263663880ba22f65b
takahiro-ueda <pt25ue@gmail.com>
2020-04-21 22:39:12 +0900

    リスト表示

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,11 @@
 //= require rails-ujs
 //= require activestorage
 //= require_tree .
+
+$(function(){
+  $('.dropdown li.hover').hover(function(){
+      $(this).children('ul').show();
+  }, function(){
+      $(this).children('ul').hide();
+  });
+});
