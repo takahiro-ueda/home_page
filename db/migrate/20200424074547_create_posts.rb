aafb1969a79f21badd4b6a340069dfaf6a06ff06
takahiro-ueda <pt25ue@gmail.com>
2020-04-24 22:11:32 +0900

    投稿index部分

---
+++ db/migrate/20200424074547_create_posts.rb
@@ -0,0 +1,13 @@
+class CreatePosts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :posts do |t|
+      t.text :title
+      t.text :content
+      t.datetime "created_at", null: false
+      t.datetime "updated_at", null: false
+      t.string "image"
+      t.integer "user_id"
+      t.timestamps
+    end
+  end
+end
