544291c08ccdafda6c0eba5c1a9f1338380a0188
takahiro-ueda <pt25ue@gmail.com>
2020-04-25 23:47:03 +0900

    トップページ新着

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root "home#index"
   
   resources :posts
-  post "posts/create"                                                                     
+                                                                       
 end
