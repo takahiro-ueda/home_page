171407fc2d33c54d80c3befcefaaaeeeaef17161
takahiro-ueda <pt25ue@gmail.com>
2020-05-13 11:47:41 +0900

    トップページ　ページネーション機能　修正

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
     @posts = Post.all
-    @posts= Post.order(created_at: :desc).page(params[:page]).per(8)
+    @posts= Post.order(created_at: :desc).page(params[:page]).per(10)
     @likes = Like.where(user_id: current_user)
   end
 
