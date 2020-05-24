4609b0c54eab790c5d696b6245ac1d6ba6325298
takahiro-ueda <pt25ue@gmail.com>
2020-05-24 17:20:58 +0900

    user 単体テスト

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,13 +9,9 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :like_posts, through: :likes, source: :post
 
-  validates :password,
-        presence: { message: '未入力' },
-        length: { in: 4..16, message: '4文字以上16文字以内でご入力ください。', allow_blank: true},
-        confirmation: { message: 'パスワードが一致しません。' }
-
-  validates :nickname, presence: true
-  validates :nickname, length: { maximum: 8 }
-  validates :password_confirmation, presence: { message: '未入力' }
-  validates :profile, length: { maximum: 1000 } 
+  validates :nickname, presence: true, length: { minimum: 1 ,maximum:20  }
+  validates :password, presence: true, length: { minimum: 6 ,maximum:128 }
+  validates :email, presence: true, format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },uniqueness: true
+  validates :age, presence: true
+  validates :sex, presence: true
 end
---
+++ spec/factories/users.rb
@@ -0,0 +1,12 @@
+FactoryBot.define do
+
+  factory :user do
+    nickname              {"abed"}
+    email                 {"kkk@gmail.com"}
+    password              {"00000000"}
+    password_confirmation {"00000000"}
+    age                   {"10"}
+    sex                   {"1"}
+  end
+
+end
