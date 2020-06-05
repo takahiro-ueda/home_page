db93f7dc74ce49930f3d275d32a12f2cbc5f4369
takahiro-ueda <pt25ue@gmail.com>
2020-06-05 23:33:54 +0900

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -17,8 +17,8 @@ module HomePage
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
-    # config.assets.initialize_on_precompile = false
-    config.action_controller.permit_all_parameters = true
+    config.assets.initialize_on_precompile = false
+    # config.action_controller.permit_all_parameters = true
 
     config.i18n.default_locale = :ja
   end
