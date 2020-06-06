4203cabdbf88225a26ff71ed9f403cf3aa472b6f
takahiro-ueda <pt25ue@gmail.com>
2020-06-07 00:46:50 +0900

    Update environment.js

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -3,9 +3,32 @@ const { environment } = require('@rails/webpacker')
 const {VueLoaderPlugin} = require('vue-loader')
 const vue = require('./loaders/vue')
 
+module.exports = {
+  module: {
+    rules: [
+      // ... other rules
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      }
+    ]
+  },
+  plugins: [
+    // make sure to include the plugin!
+    new VueLoaderPlugin()
+  ]
+}
+
+module.exports = environment
+
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
 environment.loaders.prepend('vue', vue)
 environment.config.resolve.alias = {'vue$': 'vue/dist/vue.esm.js' }
+module.exports = environment
+
+const customConfig = require('./custom')
+environment.config.merge(customConfig)
+
 // jQueryとBootstapのJSを使えるように
 const webpack = require('webpack')
 environment.plugins.prepend('Provide',
