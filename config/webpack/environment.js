7cde462b24d94b5e098902ea455130b6695a0410
takahiro-ueda <pt25ue@gmail.com>
2020-06-06 15:50:13 +0900

    Update environment.js

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -5,13 +5,13 @@ const vue = require('./loaders/vue')
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
 environment.loaders.prepend('vue', vue)
 environment.config.resolve.alias = {'vue$': 'vue/dist/vue.esm.js' }
-// jQueryとBootstapのJSを使えるように
-const webpack = require('webpack')
-environment.plugins.prepend('Provide',
-  new webpack.ProvidePlugin({
-    $: 'jquery/src/jquery',
-    jQuery: 'jquery/src/jquery',
-    Popper: 'popper.js'
-  })
-)
+// // jQueryとBootstapのJSを使えるように
+// const webpack = require('webpack')
+// environment.plugins.prepend('Provide',
+//   new webpack.ProvidePlugin({
+//     $: 'jquery/src/jquery',
+//     jQuery: 'jquery/src/jquery',
+//     Popper: 'popper.js'
+//   })
+// )
 module.exports = environment
