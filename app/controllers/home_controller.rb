fb9ebb1429e97a8311cdb64d0b6ce0ff7c8f73a5
takahiro-ueda <pt25ue@gmail.com>
2020-06-07 22:21:37 +0900

    Update home_controller.rb

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,8 +2,7 @@ class HomeController < ApplicationController
 
   def index
     # @posts = Post.all
-    @posts = Post.where(params[:category_id]).page(params[:page]).per(5)
-    # @posts = Post.includes(:user)
+    @posts = Post.includes(:user).order(created_at: :desc).page(params[:page]).per(5)
     # Categoryのデータベースのテーブルから一致するidを取得
     # @category = Category.where(params[:category_id]).page(params[:page]).per(5).order(created_at: :desc)
     # category_idと紐づく投稿を取得
