d21817c6862af94f302b96b6c5efdae816cff377
takahiro-ueda <pt25ue@gmail.com>
2020-06-08 00:17:24 +0900

    Update application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -25,21 +25,3 @@ require('jquery')
 
 require("trix")
 require("@rails/actiontext")
-
-
-$(document).on('turbolinks:load', function() {
-  $('.content__image').not('.slick-initialized').slick({
-    // arrows: false,
-    // autoplay: true,
-    // autoplaySpeed: 0,
-    // speed: 7000,
-    // cssEase: "linear",
-    // pauseOnHover: true
-
-    infinite: true, //スライドのループ有効化
-    dots:true, //ドットのナビゲーションを表示
-    centerMode: true, //要素を中央寄せ
-    centerPadding:'10%', //両サイドの見えている部分のサイズ
-    autoplay:true, //自動再生
-  });
-});
