cffa7ccb1d61f5e112a10e9f4ad81a742c478078
takahiro-ueda <pt25ue@gmail.com>
2020-06-07 21:22:14 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
-  
-  root 'home#index'
 
+  devise_for :users
+
+  root 'home#index'
   resources :users do
     collection do
       get 'profile', to: 'users#profile'
