83ca957cf3bd404752ddc4a3fe89339418aea6cf
takahiro-ueda <pt25ue@gmail.com>
2020-06-07 20:42:01 +0900

    heroku デプロイ　home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
 
   def index
     # @posts = Post.all
-    @posts = Post.order(created_at: :desc).page(params[:page]).per(5)
+    @posts = Post.all.order(created_at: :desc).page(params[:page]).per(5)
     # @posts = Post.includes(:user)
     # Categoryのデータベースのテーブルから一致するidを取得
     # @category = Category.where(params[:category_id]).page(params[:page]).per(5).order(created_at: :desc)
